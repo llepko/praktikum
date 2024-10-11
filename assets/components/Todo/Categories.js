ddc645b86b761edab42cd1e3474d7b65973a66dd
LeonidLepko <daron1988@gmail.com>
2024-10-11 14:17:00 +0200

    todo

---
+++ assets/components/Todo/Categories.js
@@ -0,0 +1,124 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Loader from "./Common/Loader";
+import "./User.css";
+const EditUser = () => {
+  const [user, setUser] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const getUserApi = "/api/users";
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = () => {
+    axios
+      .get(getUserApi.concat("/") + id)
+      .then((item) => {
+        setUser(item.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handelInput = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    console.log(name, value);
+    setUser({ ...user, [name]: value });
+  };
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(getUserApi.concat("/") + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        var json = response.json();
+
+        if (!response.ok) {
+          json.then((e) => {
+            if(e.status == 422) {
+              setError(e.detail);
+            }
+          });
+
+          throw new Error("Network response was not ok");
+        }
+        return json;
+      })
+      .then((data) => {
+        setIsLoading(true);
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      })
+  };
+
+  return (
+    <div className="user-form">
+      <div className="heading">
+      {isLoading && <Loader />}
+      {error && <p>Error: {error}</p>}
+        <p>Edit Form</p>
+      </div>
+      <form onSubmit={handelSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={user.name || ''}
+              onChange={handelInput}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="lastName" className="form-label">
+            Last Name
+          </label>
+          <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              name="lastName"
+              value={user.lastName || ''}
+              onChange={handelInput}
+          />
+        </div>
+        <div className="mb-3 mt-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={user.email || ''}
+              onChange={handelInput}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary submit-btn bg-dark">
+          EDIT
+        </button>
+      </form>
+    </div>
+  );
+};
+export default EditUser;
---
+++ src/Entity/TodoList.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TodoListRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TodoListRepository::class)]
+#[ApiResource]
+class TodoList
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
